74c8976eb363e9a2efa943983dc20d5ff4790506
Ve4erka <79873189821@yandex.ru>
2024-03-31 22:21:42 +0400

    test

--- src/components/CurrentChatPage.js
+++ src/components/CurrentChatPage.js
@@ -25,6 +25,8 @@ class CurrentChatPage extends Component {
             unread_messages: {},
             show_unread_notif: false,
             first_unread_message_id: null,
+            date_first_taken: "",
+            get_more: true,
         }
     }
 
@@ -34,61 +36,74 @@ class CurrentChatPage extends Component {
         this.client.onopen = () => {
             console.log('WebSocket Client Connected');
         };
-        this.client.onmessage = (message) => {
-            //console.log('СООБЩЕНИЕ ПРИШЕДШЕЕ', message);
-            //console.log('СООБЩЕНИЯ ДО', JSON.parse(message.data).messages);
+        this.client.onmessage = async (message) => {
             let message_parsed = JSON.parse(message.data);
             console.log(message_parsed);
-            if (message_parsed.type == "previous_messages"){
-                for (let current_message of JSON.parse(message.data).messages){
-                //console.log(current_message.message_data);
-                let parsedMessage = current_message.message_data;
-                //parsedMessage.user.avatar = "https://unimeet.ru/api/v1/media/images/test_bB0gA6Q.jpg";
-                //console.log(parsedMessage);
-                this.setState((previousState) => ({
-                    messages: GiftedChat.append(previousState.messages, parsedMessage),
-                }));
+            let count_already_read_messages = message_parsed.messages.length;
+            if (message_parsed.type == "previous_messages") {
+                if (count_already_read_messages) {
+                    this.setState({ date_first_taken: message_parsed.messages[0].message_data.createdAt });
+                    for (let current_message of message_parsed.messages) {
+                        let parsedMessage = current_message.message_data;
+                        this.setState((previousState) => ({
+                            messages: GiftedChat.append(previousState.messages, parsedMessage),
+                        }));
+                    }
                 }
-                console.log(message_parsed.unread_messages.length);
-                if(message_parsed.unread_messages.length){
-                this.setState({count_all_messages: message_parsed.unread_messages.length, show_unread_notif : true});
-                this.setState({first_unread_message_id:message_parsed.unread_messages[0].message_data._id});
-                for (let unreaded_message of message_parsed.unread_messages){
-                    console.log('Непрочитанное сообщение');
-                    console.log(unreaded_message);
-                    let current_unreaded_message = unreaded_message.message_data;
-                    current_unreaded_message["old"] = true;
+                if (message_parsed.unread_messages.length) {
+                    if (!count_already_read_messages) {
+                        this.setState({ date_first_taken: message_parsed.unread_messages[0].message_data.createdAt });
+                    }
+                    this.setState({ count_all_unread_messages: message_parsed.unread_messages.length, show_unread_notif: true });
+                    this.setState({ first_unread_message_id: message_parsed.unread_messages[0].message_data._id });
+                    for (let unreaded_message of message_parsed.unread_messages) {
+                        let current_unreaded_message = unreaded_message.message_data;
+                        current_unreaded_message["old"] = true;
+                        this.setState((previousState) => ({
+                            messages: GiftedChat.append(previousState.messages, current_unreaded_message),
+                        }));
+                    }
+                }
+            }
+            else {
+                console.log('Пришедшие сообщения новые');
+                if (message_parsed.loading_more) {
+                    if (message_parsed.messages.length) {
+                        this.setState({ date_first_taken: message_parsed.messages[message_parsed.messages.length - 1].message_data.createdAt });
+                        for (let new_message of message_parsed.messages) {
+                            //console.log(new_message);
+                            this.setState((previousState) => ({
+                                messages: GiftedChat.prepend(previousState.messages, new_message.message_data),
+                            }));
+                        }
+                    }
+                    else {
+                        this.setState({ get_more: false });
+                    }
+                }
+                else {
+                    if (!this.state.can_read) {
+                        //console.log(message_parsed);
+                        message_parsed["unread"] = true;
+                    }
+                    else {
+                        let message_id = message_parsed._id;
+                        let data = {
+                            "chat_id": this.chatId,
+                            "user_hash": this.user_hash,
+                            "message_id": message_id,
+                        }
+                        //console.log('Отправляем сообщение что оно прочитано');
+                        this.client.send(JSON.stringify({ "change_message_state": data }));
+
+                    }
                     this.setState((previousState) => ({
-                    messages: GiftedChat.append(previousState.messages, current_unreaded_message),
+                        messages: GiftedChat.append(previousState.messages, message_parsed),
                     }));
                 }
-                }
-            }
-            else{
-                console.log('А ЧЕ МЫ ДЕЛАЕМ?')
-                console.log(this.state.can_read);
-                if(!this.state.can_read){
-                //console.log(message_parsed);
-                message_parsed["unread"] = true;
-                }
-                else{
-                let chat_id = this.chatId;
-                let user_hash = this.user_hash;
-                let message_id = message_parsed._id;
-                let data = {
-                    "chat_id":chat_id,
-                    "user_hash": user_hash,
-                    "message_id": message_id,
-                }
-                console.log('Отправляем сообщение что оно прочитано');
-                this.client.send(JSON.stringify({"change_message_state" : data}));
 
-                }
-                this.setState((previousState) => ({
-                messages: GiftedChat.append(previousState.messages, message_parsed),
-                }));
             }
-        }
+        };
     }
 
     getItemLayout = (data, index) => ({
@@ -112,12 +127,38 @@ class CurrentChatPage extends Component {
     onScroll = (event) => {
         const { contentOffset } = event;
 
-        console.log('Это смещение по вертикали', contentOffset);
+        console.log('EVENT');
+        console.log(event)
+
+        let download_messages_point = 0.9 * (event.contentSize.height - event.layoutMeasurement.height);
+        console.log('Это смещение по вертикали', contentOffset.y);
 
         if (contentOffset.y < 50) {
             this.setState({ can_read: true })
         }
         else {
+
+            console.log('Можно ли загружать сообщения?')
+            console.log(this.state.get_more);
+
+            if (contentOffset.y > download_messages_point && this.state.get_more) {
+                //запросить еще сообщения
+
+                console.log('Хочу запросить еще сообщения!');
+                console.log(this.state.date_first_taken);
+
+                data_get_more = {
+                    "user_hash": "bb1b2d8081543b69fa605b9b6b00a51a18f45aa606e8189ea30eea0890ff37d5",
+                    "last_read_message_date": this.state.date_first_taken,
+                }
+                // data_get_more = JSON.stringify(data_get_more)
+                console.log(data_get_more);
+                this.setState(prevState => ({
+                    trigger_height: prevState.trigger_height + Dimensions.get('window').height / 2
+                }));
+                this.client.send(JSON.stringify({ "get_more_messages": data_get_more }));
+                this.setState({ get_more: false });
+            }
             this.setState({ can_read: false })
         }
 
@@ -178,16 +219,19 @@ class CurrentChatPage extends Component {
                             const { height } = event.nativeEvent.layout;
                             if ((props.currentMessage.new && props.currentMessage.unread) || props.currentMessage.old) {
                                 const messageId = props.currentMessage._id;
-                                console.log(messageId, 'Мессага ИД');
+                                //console.log(messageId, 'Мессага ИД');
                                 this.setState(prevState => ({
                                     unread_messages: { ...prevState.unread_messages, [messageId]: height }
                                 }), () => {
                                     // console.log('UPDATED unread_messages:', this.state.unread_messages);
                                     // console.log(count_all_messages);
                                     // console.log(Object.keys(this.state.unread_messages).length);
-                                    // if (count_all_messages == Object.keys(this.state.unread_messages).length) this.scrollToTop();
+                                    if (count_all_messages == Object.keys(this.state.unread_messages).length) this.scrollToTop();
                                     // console.log('Каво?', Object.keys(this.state.unread_messages));
                                 });
+                                if (props.currentMessage.createdAt == this.state.date_first_taken) {
+                                    this.setState({ get_more: true });
+                                }
                             }
                         }}>
                             {show_unread_notif && ((props.currentMessage.new && props.currentMessage.unread) || props.currentMessage.old) && props.currentMessage._id == first_unread_message_id ? <Text>Непрочитанные сообщения</Text> : <></>}

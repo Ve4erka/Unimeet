9a637724e193293d21693bee8d6cc4e9cabe3f3f
Ve4erka <79873189821@yandex.ru>
2024-03-31 13:24:14 +0400

    fix

--- src/components/ChatElem.js
+++ src/components/ChatElem.js
@@ -1,63 +1,86 @@
-import react, {Component} from 'react'
-import {View, Text, Image, TextInput,TouchableOpacity, StyleSheet} from 'react-native'
+import react, { Component } from 'react'
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 import { styles } from '../styles/styles';
 import { ColorsApp } from '../styles/colors';
 
 
-class ChatElem extends Component{
+class ChatElem extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        console.log('пропсы миниэлемента');
-        console.log(this.props);
-        this.data_generate_chat = this.props.data;
+        // console.log('пропсы миниэлемента');
+        // console.log(this.props);
+        console.log('CHAT ELEM');
+        console.log(this.props.data);
+        this.state = {
+            data_generate_chat: this.props.data,
+        }
+
         //this.event_name = props.data.event_name;
     }
 
-    navigateToCurrentChatPage = () => {
-        this.props.navigation.navigate('CurrentChatPage', {chat_id:this.props.data.chat_id});
+    componentDidUpdate(prevProps, prevState) {
+        console.log('ДО');
+        console.log(prevProps.data)
+        console.log('После');
+        console.log(this.props.data);
+        if (this.props.data !== prevProps.data) {
+            this.setState({ data_generate_chat: this.props.data });
+        }
     }
 
-    render(){
+    navigateToCurrentChatPage = () => {
+        this.props.navigation.navigate('CurrentChatPage', { chat_id: this.props.data.chat_id });
+    }
 
-        console.log(this.data_generate_chat);
+    render() {
 
-        return(
-            <TouchableOpacity onPress = {this.navigateToCurrentChatPage}>
-                <View style = {styles_chat.chatElem}>
-                    <Image 
+        console.log('Дата для генерации миничата');
+
+        const { data_generate_chat } = this.state;
+
+        return (
+            <TouchableOpacity onPress={this.navigateToCurrentChatPage}>
+                <View style={styles_chat.chatElem}>
+                    <Image
                         source={require('../images/logo-event.png')}
-                        style = {styles_chat.chatPhoto}
+                        style={styles_chat.chatPhoto}
                     />
-                    <View style = {styles_chat.info_last_message}>
+                    <View style={styles_chat.info_last_message}>
                         <TextInput
                             multiline={true}
-                            style={[styles_chat.chatName, {fontWeight:'bold'}]}
-                            value={this.data_generate_chat.chat_name.length > 20
-                                ? this.data_generate_chat.chat_name.slice(0, 20) + '...'
-                                : this.data_generate_chat.chat_name}
-                            editable = {false}
+                            style={[styles_chat.chatName, { fontWeight: 'bold' }]}
+                            value={data_generate_chat.chat_name.length > 20
+                                ? data_generate_chat.chat_name.slice(0, 20) + '...'
+                                : data_generate_chat.chat_name}
+                            editable={false}
                         />
                         <View>
                             <TextInput
                                 multiline={true}
-                                style={[styles_chat.chatName, {fontSize: 14}]}
-                                value={this.data_generate_chat.last_message.user.name}
-                                editable = {false}
+                                style={[styles_chat.chatName, { fontSize: 14 }]}
+                                value={data_generate_chat.last_message.user.name}
+                                editable={false}
                             />
                             <TextInput
                                 multiline={true}
-                                style={[styles_chat.chatName, {fontSize: 14, color: ColorsApp.hint_color}]}
-                                value={this.data_generate_chat.last_message.text}
-                                editable = {false}
+                                style={[styles_chat.chatName, { fontSize: 14, color: ColorsApp.hint_color }]}
+                                value={data_generate_chat.last_message.text}
+                                editable={false}
                             />
                         </View>
-                    </View>  
-                    <View style = {styles_chat.count_unread_messages}>
-                        <View style = {styles_chat.round_count}>
-                            <Text style = {styles_chat.round_text}>{this.data_generate_chat.unread_message_count}</Text>
-                        </View>
-                    </View>             
+                    </View>
+                    {
+                        data_generate_chat.unread_message_count ?
+                            <View style={styles_chat.count_unread_messages}>
+                                <View style={styles_chat.round_count}>
+                                    <Text style={styles_chat.round_text}>{data_generate_chat.unread_message_count}</Text>
+                                </View>
+                            </View>
+                            :
+                            <></>
+                    }
+
                 </View>
             </TouchableOpacity>
         )
@@ -67,59 +90,59 @@ class ChatElem extends Component{
 export default ChatElem
 
 const styles_chat = StyleSheet.create({
-    chatName:{
+    chatName: {
         color: ColorsApp.font_color,
         fontSize: 16,
-        height:18,
+        height: 18,
     },
-    chatElem:{
-        height:65,
-        width:'100%',
-        display:"flex",
-        flexDirection:'row',
-        alignItems:"center",
-        marginBottom:14,
-        borderRadius:8,
+    chatElem: {
+        height: 65,
+        width: '100%',
+        display: "flex",
+        flexDirection: 'row',
+        alignItems: "center",
+        marginBottom: 14,
+        borderRadius: 8,
         backgroundColor: ColorsApp.white,
-        borderWidth:1,
+        borderWidth: 1,
         borderColor: ColorsApp.lines_color,
-        paddingLeft:5,
+        paddingLeft: 5,
     },
-    chatPhoto:{
-        width:50,
-        height:50,
+    chatPhoto: {
+        width: 50,
+        height: 50,
     },
-    info_last_message:{
-        display:'flex',
-        flexDirection:'column',
-        height:'100%',
-        width:'71%',
-        justifyContent:"space-between",
-        paddingVertical:5,
+    info_last_message: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        width: '71%',
+        justifyContent: "space-between",
+        paddingVertical: 5,
         marginLeft: 10,
     },
-    count_unread_messages:{
-        heght:"100%",
-        display:"flex",
-        flexDirection:"row",
-        alignItems:"center",
-        justifyContent:"center",
-        width:"13%",
+    count_unread_messages: {
+        heght: "100%",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "13%",
         // backgroundColor:"red",
     },
-    round_count:{
+    round_count: {
         backgroundColor: ColorsApp.main_color,
-        borderRadius:10,
-        height:20,
-        paddingHorizontal:7,
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'center',
-        width:"auto",
+        borderRadius: 10,
+        height: 20,
+        paddingHorizontal: 7,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: "auto",
     },
-    round_text:{
-        color:ColorsApp.white,
-        fontSize:14,
-        lineHeight:16,
+    round_text: {
+        color: ColorsApp.white,
+        fontSize: 14,
+        lineHeight: 16,
     }
 })
--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,5 +1,5 @@
-import react, {Component} from 'react'
-import {View, Text, Button, Image, TextInput,TouchableOpacity, ImageBackground, BackHandler, SafeAreaView, ActivityIndicator, FlatList, StyleSheet} from 'react-native'
+import react, { Component } from 'react'
+import { View, Text, Button, Image, TextInput, TouchableOpacity, ImageBackground, BackHandler, SafeAreaView, ActivityIndicator, FlatList, StyleSheet } from 'react-native'
 import { styles } from '../styles/styles';
 import { ScrollView } from 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -7,106 +7,123 @@ import { Svg, SvgUri } from 'react-native-svg';
 import EventElem from './EventElem';
 import { Address } from '../API/System/config';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { ColorsApp } from '../styles/colors';
 import ChatElem from './ChatElem';
 import { Dimensions } from 'react-native';
 
 
-class Chats extends Component{
+class Chats extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.backHandler = null;
         this.state = {
-            chats:[],
-            isLoading:true,
+            chats: [],
+            isLoading: true,
         }
         this.user_hash = this.props.user_data.user_hash;
         console.log('ШЫРИНА')
         console.log(Dimensions.get('window').width);
     }
 
-    componentDidMount(){
-        
+    componentDidMount() {
+
         this.client = new W3CWebSocket(`ws://unimeet.ru/ws/chat-list/?user_hash=${this.user_hash}`);
 
         this.client.onopen = () => {
             console.log('WebSocket Client Connected');
         };
 
-        this.client.onmessage =  (message) => {
+        this.client.onmessage = (message) => {
+            console.log('Сообщения из CHATS');
             console.log(message);
-            this.setState({isLoading:false})
-            let chats = JSON.parse(message.data).user_chat_list;
-            console.log(chats);
-            this.setState({chats:chats});
+            const newChats = JSON.parse(message.data).user_chat_list;
+            console.log('То что прокидываем в State');
+            console.log(newChats);
+
+            this.setState({
+                isLoading: false,
+                chats: newChats.slice() // создаем новый массив, чтобы обновить FlatList
+            });
+        };
+    }
+
+    renderChats(isLoading, chats) {
+
+        console.log('Вызвался?');
+        console.log(isLoading);
+        console.log(chats);
+        if (isLoading) {
+            return <ActivityIndicator color={ColorsApp.main_color} size="large" />
+        }
+        else {
+            console.log("Перерисовываем ъуъ")
+            return <FlatList
+                overScrollMode="never"
+                showsVerticalScrollIndicator={false}
+                data={chats}
+                keyExtractor={item => item["chat_id"]}
+                renderItem={({ item }) => (
+                    <ChatElem
+                        data={item}
+                        navigation={this.props.navigation}
+                    />
+                )}
+            />
         }
     }
 
-    
-    loadEventsScene(){
+
+    loadEventsScene() {
         this.props.navigation.navigate('Events');
     }
-    loadProfileScene(){
+    loadProfileScene() {
         this.props.navigation.navigate('Profile');
     }
 
-    render(){
+    render() {
 
-        const {chats, isLoading} = this.state;
+        const { chats, isLoading } = this.state;
+        console.log('Генерация списка');
+        console.log(chats);
 
-        return(
-            <GestureHandlerRootView style = {{flex:1}}>
-            <View style = {[styles.container, {paddingTop:30}]}>
-                <View style = {[styles.screenContent, chats_styles.mainContainer]}>
-                    <Text style = {styles.screenTitle}>Чаты</Text>
-                    <View style = {[styles.eventsContent, {}]}>
-                        {isLoading ? (
-                            <ActivityIndicator color = {ColorsApp.main_color} size = "large"/>
-                            ) : (
-                            <FlatList
-                                overScrollMode="never"
-                                showsVerticalScrollIndicator={false}
-                                data={chats}
-                                keyExtractor={item => item["chat_id"]}
-                                renderItem={({item}) => (
-                                    <ChatElem
-                                        data = {item}
-                                        navigation = {this.props.navigation}
-                                    />
-                                )}
-                            />
-                        )}
-                    </View>
-                    <View style={styles.footer}>
-                        <TouchableOpacity onPress = {()=>{this.loadEventsScene()}}>
+        return (
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <View style={[styles.container, { paddingTop: 30 }]}>
+                    <View style={[styles.screenContent, chats_styles.mainContainer]}>
+                        <Text style={styles.screenTitle}>Чаты-хуяты</Text>
+                        <View style={[styles.eventsContent, {}]}>
+                            {this.renderChats(isLoading, chats)}
+                        </View>
+                        <View style={styles.footer}>
+                            <TouchableOpacity onPress={() => { this.loadEventsScene() }}>
+                                <Image
+                                    source={require('../images/eventsIcon.png')}
+                                    style={styles.footerImg}
+                                />
+                            </TouchableOpacity>
                             <Image
-                                source={require('../images/eventsIcon.png')}
-                                style = {styles.footerImg}
+                                style={styles.footerImg}
+                                source={require('../images/messagesIcon.png')}
                             />
-                        </TouchableOpacity>
-                        <Image
-                            style = {styles.footerImg}
-                            source={require('../images/messagesIcon.png')}
-                        />
-                        <TouchableOpacity onPress = {()=>{this.loadProfileScene()}}>
-                            <Image
-                                source={require('../images/userIcon.png')}
-                                style = {styles.footerImg}
-                            />
-                        </TouchableOpacity>
+                            <TouchableOpacity onPress={() => { this.loadProfileScene() }}>
+                                <Image
+                                    source={require('../images/userIcon.png')}
+                                    style={styles.footerImg}
+                                />
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </View>
-            </View>
             </GestureHandlerRootView>
         )
     }
 }
 
 const chats_styles = StyleSheet.create({
-    mainContainer:{
-        paddingHorizontal:10,
+    mainContainer: {
+        paddingHorizontal: 10,
     },
 })
 
@@ -115,18 +132,18 @@ const mapStateToProps = (state) => {
     console.log('STATE');
     console.log(state);
     return {
-        "user_data":{
+        "user_data": {
             user_name: state.user_name, //initailState
             user_age: state.user_age,
-            user_img:state.user_img,
-            user_education:state.user_education,
-            user_description:state.user_description,
+            user_img: state.user_img,
+            user_education: state.user_education,
+            user_description: state.user_description,
             user_hash: state.user_hash,
         }
     }
 }
 const mapDispatchToProps = (dispatch) => {
-    return { 
+    return {
         updateUserData: (parameter) => {
             dispatch(uploadUserData(parameter))
         },
